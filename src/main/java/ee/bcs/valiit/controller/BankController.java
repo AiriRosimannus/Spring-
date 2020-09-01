58d9a668a831aff7120f51d328e53db47df3c9ba
opilane <airi@rosimannus.ee>
2020-09-01 16:03:58 +0300

    Controller harjutused, 1.09

--- src/main/java/ee/bcs/valiit/controller/BankController.java
+++ src/main/java/ee/bcs/valiit/controller/BankController.java
@@ -2,6 +2,7 @@ package ee.bcs.valiit.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +14,12 @@ public class BankController {
     public void addaccount(@RequestBody Account account) {
         map.put(account.getAccountNr(), account.getAmount());
     }
+
+  /*  @PutMapping("account/{amount}")
+    public void updateAmount(@PathVariable("amount") int amount, @RequestBody Account account) {
+        map.set(amount, map);
+
+    }*/
 
     @GetMapping("account/{accountNr}")
     public Integer getaccount(@PathVariable("accountNr") String account) {
---
+++ src/main/java/ee/bcs/valiit/controller/Test2Controller.java
@@ -0,0 +1,86 @@
+package ee.bcs.valiit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test2")
+public class Test2Controller {
+
+    // 1 2 3 4 5 6 7 8 9 10
+    // 2 3 4 5 6 7 8 9 10 11
+    // 3 4 5 6 7 8 9 10 11 12
+    // 10 rida
+
+    int x = 10;
+    int y = 10;
+
+    @GetMapping("y1")
+    public void test1() {
+
+        for (int x1 = 1; x1 <= x; x1++) {
+            for (int y1 = 1; y1 <= y; y1++) {
+
+                System.out.print(x1 + (y1 - 1) + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    // loe need 3 muutujat sisse kasutades request parameetrit
+    // tee nii, et tagastatav väärtus on alati miinus märgiga
+    @GetMapping("y2")
+    public void test2(@RequestParam("firstnumber") int a,
+                      @RequestParam("secondnumber") int b,
+                      @RequestParam("thirdnumber") int c) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        if (a * b * c > 0) {
+            System.out.println(-1 * (a * b * c));
+        } else if (a * b * c == 0) {
+            System.out.println("-0");
+        } else {
+            System.out.println(a * b * c);
+        }
+        // näiteks: http://localhost:8080/test2/y2?firstnumber=-2&secondnumber=3&thirdnumber=4
+        // TODO loo 3 int tüüpi muutujat
+        // Anna neile väärtused 5, 9 ,7
+        // trüki välja nende numbrite korrutis miinus märgiga
+        /*int a = 5;
+        int b = 9;
+        int c = 7;
+        System.out.println(-(a*b*c));*/
+    }
+
+    // Loed läbi requestparami 2 väärtust x, y
+    // teed tabeli mis on x ühikut lai ja y ühikut kõrge
+    // Näide:
+    // 1 2 3 4 5 6 7
+    // 2 3 4 5 6 7 8
+    // 3 4 5 6 7 8 9
+    @GetMapping("y3")
+    public void test2(@RequestParam("width") int x,
+                      @RequestParam("height") int y) {
+        for (int x1 = 1; x1 <= x; x1++) {
+            for (int y1 = 1; y1 <= y; y1++) {
+                System.out.print(x1 + (y1 - 1) + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    // sisend on int n; väljund n-is arv jadas
+    // fun(n) = fun(n-1) + fun(n-3)
+    // 0
+    // 1
+    // 1
+    // 1
+    // 2
+    // 3
+    // 4
+    // 6
+}
+
